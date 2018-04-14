47f24b4bce024934c41be0134ac9d840ca5b1711
Torrey Leonard <torreyleonard@voxela.com>
2018-04-14 13:21:04 -0400

    Delete .pre_commit.js

--- .pre_commit.js
+++
@@ -1,5 +0,0 @@
-const jsdoc2md = require('jsdoc-to-markdown');
-const fs = require('fs');
-
-jsdoc2md.render({ files: 'objects/robinhood/*' }).then(output => fs.writeFile('docs/ROBINHOOD.md', output));
-jsdoc2md.render({ files: 'objects/data/*' }).then(output => fs.writeFile('docs/DATA.md', output));
